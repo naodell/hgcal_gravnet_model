a23cab929780de6e66e5971ad86edde59102b4ad
Nathaniel Jay Odell <naodell@gmail.com>
2021-10-26 13:57:58 -0500

    Updates to plotting scripts

--- scripts/plot_to_files.py
+++ scripts/plot_to_files.py
@@ -1,3 +1,6 @@
+
+import argparse
+
 import torch
 from torch_geometric.data import DataLoader
 import numpy as np
@@ -7,6 +10,7 @@ from torch_cmspepr.dataset import TauDataset
 from torch_cmspepr.gravnet_model import GravnetModel
 import torch_cmspepr.objectcondensation as oc
 import scripts.plotting as plotting
+from scripts.helper_funcs import convert_parallel_model
 
 def pred_plot(event, out, tbeta=.1, td=1.):
     betas = torch.sigmoid(out[:,0]).numpy()
@@ -26,17 +30,29 @@ def pred_clusterspace_plot(event, out, tbeta=.1, td=1.):
     clustering = oc.get_clustering_np(betas, cluster_space_coords, tbeta=tbeta, td=td)
     return plotting.get_plotly_clusterspace(event, out[:,1:], clustering)
 
-def main():
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print more output')
+    parser.add_argument('-c', '--checkpoint', 
+            help    = 'Load checkpoint file with model weights.',
+            default = 'remote_checkpoints/ckpt_best_new.pth.tar',
+            type    = str
+            )
+    args = parser.parse_args()
+
     _, test_dataset = TauDataset('local_data/npzs_all').split(.8)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     model = GravnetModel(input_dim=9, output_dim=4)
-    ckpt = 'checkpoints/ckpts_gravnet_Sep13_2022/ckpt_32.pth.tar'
-    model.load_state_dict(torch.load(ckpt, map_location=torch.device('cpu'))['model'])
+    ckpt = os.path(f'remote_checkpoints/{args.checkpoint}')
+    state_dict = torch.load(ckpt, map_location=torch.device('cpu'))['model']
+    state_dict = convert_parallel_model(state_dict)
+    model.load_state_dict(state_dict)
 
-    tbeta = .2
-    td = .1
-    nmax = 10
+    tbeta = .1
+    td = .5
+    nmax = 20
 
     desc_str = f'tbeta{tbeta:.1f}_td{td:.1f}'.replace('.', 'p')
 
@@ -45,7 +61,7 @@ def main():
         for i, data in tqdm.tqdm(enumerate(test_loader), total=nmax):
             if i == nmax: break
             out = model(data.x, data.batch)
-            outfile = f'plots_%b%d_{desc_str}/{i:03d}.html'
+            outfile = f'plots_html/%b%d_{desc_str}/{i:03d}.html'
             plotting.write_html(
                 outfile,
                 plotting.side_by_side_html(
@@ -61,7 +77,3 @@ def main():
                     ),
                 mode = 'a'
                 )
-            
-
-if __name__ == "__main__":
-    main()
